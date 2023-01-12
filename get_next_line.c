c8b87229f5b212b42650c3f29def8ab8aab7b664
Nikolaj Postnikov <nipostni@c2r4s2.42prague.com>
2023-01-12 14:31:24 +0100

    fixed segfault, made array dynamic

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nipostni <nipostni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 13:44:35 by nipostni          #+#    #+#             */
-/*   Updated: 2023/01/12 13:26:50 by nipostni         ###   ########.fr       */
+/*   Updated: 2023/01/12 14:30:43 by nipostni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,27 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 char *get_next_line(int fd)
-{
-    char    *buf[500];
-    printf("%d\n", fd);
-    read(fd, buf, 500);
-    printf("%s", buf);
+{   
+    char    *buf;
+    char    *line[100];
+    int     i = 0;
+
+    buf = (char *)malloc(100 * sizeof(char));
+    buf[101] = '\0';
+    
+
+    read(fd, buf, 100);
+    while (buf[i] != '\n')
+    {
+        write(1, &buf[i], 1);
+        i++;
+    }
+    
+    // printf("%s", buf);
+    
 }
 
 
