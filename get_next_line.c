b035ca5846a356d89c5f372c17909a361093ec9a
Nikolaj Postnikov <nipostni@c2r4s2.42prague.com>
2023-01-13 17:58:51 +0100

    re

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nipostni <nipostni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 13:44:35 by nipostni          #+#    #+#             */
-/*   Updated: 2023/01/13 17:44:40 by nipostni         ###   ########.fr       */
+/*   Updated: 2023/01/13 17:58:28 by nipostni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@
 
 char *get_next_line(int fd)
 {   
-    static int offset = 0;
+    char *buf;
     char *c;
-    int i = 0;
+    int len = 0;
     while (read(fd, &c, 1))
     {
-        printf("%c", c);
-        i++;
+        write(1, &c, 1);
+        len++;
     }
-    
+    printf("\n%d\n", len);  
     
     
 }
