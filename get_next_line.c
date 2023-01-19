d179c258da7d5c43e85934f87df529ba7778955f
Nipostni <awis@me.com>
2023-01-19 17:30:01 +0100

    remix

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nipostni <nipostni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nipostni <awis@me.com>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 13:44:35 by nipostni          #+#    #+#             */
-/*   Updated: 2023/01/13 17:58:28 by nipostni         ###   ########.fr       */
+/*   Updated: 2023/01/19 17:23:18 by nipostni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFF_SIZE 32
+
 char *get_next_line(int fd)
-{   
-    char *buf;
-    char *c;
-    int len = 0;
-    while (read(fd, &c, 1))
-    {
-        write(1, &c, 1);
-        len++;
-    }
-    printf("\n%d\n", len);  
-    
-    
-}
-
-
-int main(void)
 {
-    int     fd;
-    
-    fd = open("file.txt", O_RDONLY);
-    get_next_line(fd);
-    get_next_line(fd);
-    get_next_line(fd);
-    get_next_line(fd);
-    get_next_line(fd);
+    static char buffer[BUFF_SIZE + 1];
+    static int offset;
+    static int bytes_read;
+    int i;
+    char *line;
+	// char *c;
+
+	line = NULL;
+	i = 0;
+	
+    if (fd < 0)
+        return NULL;    
+    if (!(line = (char*)malloc(sizeof(char))))
+		return NULL;
+	while (42)
+	{
+		if (offset == bytes_read)
+		{
+			bytes_read = read(fd, buffer, BUFF_SIZE);
+			offset = 0;
+			if (bytes_read == 0)
+			{
+				return NULL;
+				free(line);
+			}
+		}
+		line[i] = buffer[offset];
+		offset++;
+		if (line[i] == '\n')
+			break;
+	}
+	line[i] = '\n';
+	i++;
+	line[i] = '\0';
+	return (line);
 
 }
+
+
+
+
+// int main(void)
+// {
+//     int     fd;
+    
+//     fd = open("file.txt", O_RDONLY);
+
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+// 	// get_next_line(fd);
+// 	// get_next_line(fd);
+// 	// get_next_line(fd);
+// 	// get_next_line(fd);
+
+// }
